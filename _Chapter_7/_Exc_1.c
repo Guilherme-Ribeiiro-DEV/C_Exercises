50d8b045bd2a8443f1a56a36b4ef61be6be84d98
Guilherme Ribeiro <emanuelguilherme000@gmail.com>
2024-04-22 20:00:32 -0300

    Ends chapter 7 execises!

--- _Chapter_7/_Exc_1.c
+++ _Chapter_7/_Exc_1.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 
 int main(){
+    
     int age;
-    printf("Input your age: \n");
+    printf("Input your age: ");
     scanf("%d", &age);
+    
+    if (age % 2 == 0 ) {
+        printf("Your age is PAR!\n");
+        printf("Your age multiplied by two: %d\n", age * 2);
+        printf("Your age divided by two: %d\n", age / 2);
+    }
+    else {
+        printf("Your age is ÍMPAR!\n");
+    }
+
     if (age < 18) {
         if (age > 16) {
             printf("You already can vote!");
         }
         printf("Minimum age not reached!");
-    } else if (age > 65) {
+    }
+    else if (age > 65) {
         printf("Maximum age reached! Do you wanna vote anyway?");
-    } else {
+    }
+    else {
         printf("Minimum age reached, you should vote!");
     }
 }
